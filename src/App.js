9325163145399d762dae829224e4e21062d9e729
Jared Chavez <mereditirri1624@gmail.com>
2021-10-23 00:22:48 -0500

    Save changes

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="overlay">
+        <div className="App-header">
+          <div className="title-container">
+              <h2>Visual Rhythms</h2>
+            </div>
+              <ul>
+                <li>About</li>
+                <li>Contact Us</li>
+              </ul>
+        </div>
+
+        <div className="info-contact">
+          <div className="social-container">
+            
+          </div>
+        </div>
+
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
